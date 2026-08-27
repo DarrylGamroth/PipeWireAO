0a1e14e83e1551bfe0cccddb04d5813dec0a1d58
Darryl Gamroth <dgamroth@rubus.ca>
2026-08-27 10:43:58 -0700

    Restore per-port ndarray profile negotiation

--- spa/include/spa/param/format-types.h
+++ spa/include/spa/param/format-types.h
@@ -236,6 +236,7 @@ static const struct spa_type_info spa_type_format[] = {
 		spa_type_ndarray_layout },
 	{ SPA_FORMAT_NDARRAY_rate, SPA_TYPE_Fraction, SPA_TYPE_INFO_FORMAT_NDARRAY_BASE "rate", NULL },
 	{ SPA_FORMAT_NDARRAY_schema, SPA_TYPE_String, SPA_TYPE_INFO_FORMAT_NDARRAY_BASE "schema", NULL },
+	{ SPA_FORMAT_NDARRAY_profile, SPA_TYPE_String, SPA_TYPE_INFO_FORMAT_NDARRAY_BASE "profile", NULL },
 
 	{ SPA_FORMAT_VIDEO_deviceId, SPA_TYPE_Bytes, SPA_TYPE_INFO_FORMAT_BASE "deviceId", NULL },
 	{ 0, 0, NULL, NULL },
